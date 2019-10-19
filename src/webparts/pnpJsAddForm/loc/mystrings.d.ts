d055e0c07cb12dd6f5326b43bf4ec1960c208cb8
Anurag Asthana <a.asthana90@gmail.com>
2019-10-19 17:48:50 +0530

    the first commit

---
+++ src/webparts/pnpJsAddForm/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IPnpJsAddFormWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'PnpJsAddFormWebPartStrings' {
+  const strings: IPnpJsAddFormWebPartStrings;
+  export = strings;
+}
